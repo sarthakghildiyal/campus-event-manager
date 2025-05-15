c034edf7e28ec30fce2f9f457fe7be5f898d9d87
Kaustubh Kale <s224942922@deakin.edu.au>
2025-05-15 17:18:26 +1000

    Initial Node.js setup -Kaustubh Kale

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
